e443b3e86609c8a47ae156b508e795e7fccfd805
ahmednour.belkziz <ahmednour.belkziz@etu.estia.fr>
2025-02-06 14:53:15 +0100

    added static gateway

--- customer/src/main/java/fr/estia/mbds/customer/CustomerController.java
+++ customer/src/main/java/fr/estia/mbds/customer/CustomerController.java
@@ -1,5 +1,6 @@
 package fr.estia.mbds.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class CustomerController {
+public class    CustomerController {
     private final CustomerRepository customerRepository;
-
+    @Autowired
     public CustomerController(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
